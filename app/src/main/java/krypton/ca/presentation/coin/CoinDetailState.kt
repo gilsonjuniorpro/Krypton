ef84b896e16d1756cdbbe9438c108d74b0a0eb08
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2022-05-16 04:15:34 -0400

    create detail state and view model

---
+++ app/src/main/java/krypton/ca/presentation/coin/CoinDetailState.kt
@@ -0,0 +1,9 @@
+package krypton.ca.presentation.coin
+
+import krypton.ca.domain.model.CoinDetail
+
+data class CoinDetailState(
+    val isLoading: Boolean = false,
+    val coin: CoinDetail? = null,
+    val error: String = ""
+)
